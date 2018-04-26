08e89001a16fda2c294d946234d804cebcfca807
Atirek Sharma <atireksharma@atirekmacbook.local>
2018-04-26 16:47:33 +0530

    Initial Commit

---
+++ APIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIDemo
+//
+//  Created by Atirek Sharma on 26/04/18.
+//  Copyright © 2018 Atirek Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
